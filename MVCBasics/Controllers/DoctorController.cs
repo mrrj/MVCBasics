1b1dd685129dfe81a48c71b9f19a5d659c161cfc
Marja Rolandsson <marja.rolandsson@gmail.com>
2021-08-23 19:35:32 +0200

    Upload for part 2

---
+++ MVCBasics/Controllers/DoctorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MVCBasics.Models;
+
+namespace MVCBasics.Controllers
+{
+    public class DoctorController : Controller
+    {
+        public IActionResult FeverCheck(string temperature)
+        {
+            if (temperature != null)
+            {
+                double temp = double.Parse(temperature);
+                if (Doctor.HasFever(temp)) ViewBag.Message = "You have a fever";
+                else ViewBag.Message = "You do not have a fever";
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCBasics/Models/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasics.Models
+{
+    public class Doctor
+    {
+        public static bool HasFever(double temperature)
+        {
+            if (temperature > 37.5) return true;
+            else return false;
+
+        }
+
+    }
+}
